551bf581ceb417dda33b08978e37483331483eeb
Shiv Kumar <yourshivkumar17@gmail.com>
2026-07-17 11:24:24 +0530

    Fix MajorityElement imports and compilation

---
+++ MajorityElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MajorityElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n/2]);
+        sc.close();
+    }
+
+}
